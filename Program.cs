b8b86d5b0a8c028aa3f26862b4b990fc25c257ed
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-01-30 03:14:10 +0530

    Added :Program file

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Sanke_And_Lader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Snake & Ladder\r\nSimulator");
+       
+        
+        }
+    }
+}
