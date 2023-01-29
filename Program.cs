54ed75bc44b41370d40ae47cad65f2ae11130d0f
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-01-30 04:47:26 +0530

    Added:UC1 program file intialize

--- Program.cs
+++ Program.cs
@@ -7,8 +7,14 @@ namespace Sanke_And_Lader
         static void Main(string[] args)
         {
             Console.WriteLine("Snake & Ladder\r\nSimulator");
-       
-        
+
+            Console.WriteLine("Snake and Ladder\ngame played with single player at start\r\nposition 0");
+
+
+            int position = 0;
+            int step = 0;
+          
+
         }
     }
 }
