9db6c841582d17091300395e19c16808371c5e4d
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-01-30 05:06:26 +0530

    Added:UC2 program file impliment the dice by random

--- Program.cs
+++ Program.cs
@@ -6,13 +6,17 @@ namespace Sanke_And_Lader
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Snake & Ladder\r\nSimulator");
+            Console.WriteLine("******Snake & Ladder Simulator******");
 
             Console.WriteLine("Snake and Ladder\ngame played with single player at start\r\nposition 0");
 
 
             int position = 0;
             int step = 0;
+            Random obj=new Random();
+            int dice = obj.Next(1,7);
+
+            Console.WriteLine(dice);
           
 
         }
