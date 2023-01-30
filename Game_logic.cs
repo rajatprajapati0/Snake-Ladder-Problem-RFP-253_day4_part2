9d3e153a3052c80283860a03161b0fce8de69085
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-01-30 15:43:29 +0530

    Added:finle program file wiht implementation of tow players and with a class file wich have logic (Game_logic.cs) in UC7

---
+++ Game_logic.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sanke_And_Lader
+{
+    internal class Game_logic
+    {
+        public int logic()
+        {
+            int diceCount = 0;
+            int position = 0;
+            int step = 0;
+            Random obj = new Random();
+
+            while (step < 100)
+            {
+                int dice = obj.Next(1, 7);
+                Console.WriteLine("you got on dice " + dice);
+                int option = obj.Next(0, 3);
+
+                switch (option)
+                {
+                    case 0:
+                        Console.WriteLine(" but no play");
+                        position = position + 0;
+                        Console.WriteLine("your position is" + position);
+                        step = position;
+                        diceCount++;
+                        break;
+                    case 1:
+                        Console.WriteLine("ladder - go ahed");
+                        position = position + dice;
+                        step = position;
+
+                        if (step > 100)
+                        {
+                            position = position - dice;
+                            Console.WriteLine("you can not move more than 100 step dice again yor position is " + position);
+                            step = position;
+                        }
+                        else
+                        {
+                            Console.WriteLine("your position " + position);
+                            step = position;
+
+                        }
+
+                        diceCount++;
+
+                        break;
+                    case 2:
+
+                        Console.WriteLine("snake - go back");
+
+                        position = position - dice;
+                        step = position;
+
+                        if (step < 0)
+                        {
+                            position = 0;
+                            step = position;
+                        }
+                        else
+                        {
+                            Console.WriteLine("your position " + position);
+                            step = position;
+                        }
+                        diceCount++;
+                        break;
+
+
+                }
+            }
+
+            Console.WriteLine("you played the dice " + diceCount + " times");
+
+            return diceCount;
+        } 
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,73 +7,40 @@ namespace Sanke_And_Lader
         static void Main(string[] args)
         {
             Console.WriteLine("******Snake & Ladder Simulator******");
-            Console.WriteLine("Snake and Ladder\ngame played with single player at start\r\nposition 0");
-            int diceCount = 0;
-            int position = 0;
-            int step = 0;
-            Random obj=new Random();
-        
-           while (step < 100)
-           {
-                   int dice = obj.Next(1, 7);
-                   Console.WriteLine("you got on dice " + dice);
-                   int option = obj.Next(0, 3);
+            Console.WriteLine("Snake and Ladder\ngame played with two players at start\r\nposition 0");
 
-             switch (option)
-             {
-                    case 0:
-                        Console.WriteLine(" but no play");
-                        position = position + 0;
-                        Console.WriteLine("your position is" + position);
-                        step = position;
-                        diceCount++;
-                        break;
-                    case 1:
-                        Console.WriteLine("ladder - go ahed");
-                        position = position + dice;
-                        step = position;
-                       
-                        if (step > 100)
-                        {
-                            position = position - dice;
-                            Console.WriteLine("you can not move more than 100 step dice again yor position is " + position);
-                            step = position;
-                        }
-                        else
-                        {
-                            Console.WriteLine("your position " + position);
-                            step = position;
-                            
-                        }
+            int resultOFplayer1 = 0;
+            int resultOFplayer2 = 0;
+           Game_logic obj= new Game_logic();
+            for (int i = 1; i <= 2; i++) 
+            {
+             if (i==1)
+                {
+                    Console.WriteLine("**********player one**********\n");
+                    int player1 = obj.logic();
+                    resultOFplayer1 = player1;
+                }
 
-                           diceCount++;
+             else
+                {
+                    Console.WriteLine("\n********player two********\n");
+                    int player2 = obj.logic();
+                    resultOFplayer2 = player2;
+                }
+            }
+            Console.WriteLine("  result OF player 1 -" + resultOFplayer1);
+            Console.WriteLine("  result OF player 2 -" + resultOFplayer2);
+            if (resultOFplayer1 < resultOFplayer2)
+            {
+                Console.WriteLine("*****player one is winner*****");
 
-                        break;
-                    case 2:
-                       
-                        Console.WriteLine("snake - go back");
+            }
+            else 
+            {
+                Console.WriteLine("*****player two is winner*****");
 
-                              position = position - dice;
-                              step = position;
-      
-                        if (step<0) 
-                        {
-                            position = 0;
-                            step = position;
-                        }
-                        else
-                        {
-                            Console.WriteLine("your position " + position);
-                            step = position;
-                        }
-                        diceCount++;
-                        break;
+            }
 
-                
-             }
-           }
-
-            Console.WriteLine("you played the dice "+diceCount+" times");
         }
     }
 }
